0e73e9030232516f7d079b191713f9ad901d7484
Nuria Campo <nuria.campo7@gmail.com>
2024-11-07 10:41:20 +0100

    replaces empty image

--- js/main.js
+++ js/main.js
@@ -10,17 +10,33 @@ const handleSearch = () => {
   fetch(`https://api.jikan.moe/v4/anime?q=${inputValue}`)
     .then((response) => response.json())
     .then((data) => {
+      // console.log("data es", data);
       const series = data.data;
-
+      // console.log("series es", series);
       for (const serie of series) {
-        console.log("serie es", serie);
+        /*Cuántas páginas hay
+        const totalPages = data.pagination.items.total;
+        const seriePerPage = data.pagination.items.per_page;
+        const pages = Math.ceil(totalPages / seriePerPage);
+        console.log("Hay", pages, "páginas");
+        */
+
+        // Si no hay imagen en el listado, pinta la imagen de TV, sino, pinta la imagen que viene en el listado
+        const urlImage =
+          serie.images.jpg.image_url ===
+          "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"
+            ? "https://via.placeholder.com/210x295/ffffff/666666/?text=TV"
+            : serie.images.jpg.image_url;
+
+        const titleSerie = serie.title;
+
         results.innerHTML += `
         <li>
             <img
-              src=${serie.images.jpg.image_url}
+              src=${urlImage}
               alt=""
             />
-            <p>${serie.title}</p>
+            <p>${titleSerie}</p>
           </li>
         `;
       }
