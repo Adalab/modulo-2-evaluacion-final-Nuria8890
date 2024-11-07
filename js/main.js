46b999139684e8a7a9ea60ecfd6d0c066a1cbf78
Nuria Campo <nuria.campo7@gmail.com>
2024-11-07 13:01:37 +0100

    reorganize fetch

--- js/main.js
+++ js/main.js
@@ -4,33 +4,18 @@ const button = document.querySelector(".js-button");
 const input = document.querySelector(".js-input");
 const results = document.querySelector(".js-results");
 const favorites = document.querySelector(".js-favorites");
+let seriesData = [];
 let favoritesSeries = [];
 
 const renderSeries = (series) => {
   for (const serie of series) {
-    /*Cuántas páginas hay
-    const totalPages = data.pagination.items.total;
-    const seriePerPage = data.pagination.items.per_page;
-    const pages = Math.ceil(totalPages / seriePerPage);
-    console.log("Hay", pages, "páginas");
-    */
-
-    // Si no hay imagen en el listado, pinta la imagen de TV, sino, pinta la imagen que viene en el listado
-    const urlImage =
-      serie.images.jpg.image_url ===
-      "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"
-        ? "https://via.placeholder.com/210x295/ffffff/666666/?text=TV"
-        : serie.images.jpg.image_url;
-
-    const titleSerie = serie.title;
-
     results.innerHTML += `
-        <li class="js-serie">
+        <li class="js-serie" id = ${serie.idSerie}>
             <img
-              src=${urlImage}
+              src=${serie.urlImage}
               alt=""
             />
-            <p>${titleSerie}</p>
+            <p>${serie.titleSerie}</p>
           </li>
         `;
   }
@@ -63,6 +48,7 @@ const handleFavorites = (event) => {
   renderSeriesFavorites(favoritesSeries);
 };
 
+// 2. Búsqueda
 const handleSearch = () => {
   const inputValue = input.value;
 
@@ -71,9 +57,33 @@ const handleSearch = () => {
     .then((data) => {
       // console.log("data es", data);
       const series = data.data;
-      // console.log("series es", series);
+      console.log("series es", series);
 
-      renderSeries(series);
+      for (const serie of series) {
+        /*Cuántas páginas hay
+        const totalPages = data.pagination.items.total;
+        const seriePerPage = data.pagination.items.per_page;
+        const pages = Math.ceil(totalPages / seriePerPage);
+        console.log("Hay", pages, "páginas");
+        */
+
+        // Si no hay imagen en el listado, pinta la imagen de TV, sino, pinta la imagen que viene en el listado
+        const urlImage =
+          serie.images.jpg.image_url ===
+          "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"
+            ? "https://via.placeholder.com/210x295/ffffff/666666/?text=TV"
+            : serie.images.jpg.image_url;
+
+        const titleSerie = serie.title;
+        const idSerie = serie.mal_id;
+        seriesData.push({
+          urlImage,
+          titleSerie,
+          idSerie,
+        });
+      }
+      console.log(seriesData);
+      renderSeries(seriesData);
       addFavoritesSeries();
     });
 };
